eefb9888ff6b441eef89082170a21c8747b29e28
robinrdj <robinrajaduraij@gmail.com>
2025-05-09 22:42:16 +0530

    checking commit

--- index.js
+++ index.js
@@ -5,8 +5,13 @@ require("dotenv").config();
 const app = express();
 app.use(cors());
 app.use(express.json());
+
+//for checking
 app.get("/", (req, res) => {
   res.send("backend is running");
+});
+app.get("/check", (req, res) => {
+  res.send("checking");
 });
 app.use("/api/users", require("./routes/users"));
 app.use("/api/tasks", require("./routes/tasks"));
